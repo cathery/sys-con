b20116ef5e59bcf0bbeb159954cc703d1f79d9a0
cathery <dumdumcathery@gmail.com>
2020-05-28 14:53:19 +0300

    Update libnx, libstrat, reduce FS sessions

--- source/Sysmodule/source/main.cpp
+++ source/Sysmodule/source/main.cpp
@@ -12,7 +12,12 @@
 // libnx fake heap initialization
 extern "C"
 {
+    // We aren't an applet, so disable applet functionality.
     u32 __nx_applet_type = AppletType_None;
+    // We are a sysmodule, so don't use more FS sessions than needed.
+    u32 __nx_fs_num_sessions = 1;
+    // We don't need to reserve memory for fsdev, so don't use it.
+    u32 __nx_fsdev_direntry_cache_size = 1;
 
 #define INNER_HEAP_SIZE 0x40'000
     size_t nx_inner_heap_size = INNER_HEAP_SIZE;
